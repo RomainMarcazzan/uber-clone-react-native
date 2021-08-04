fd048fb1f85205c0773f4825bd82eb08468ae8cd
RomainMarcazzan <romain.marcazzan@hotmail.com>
2021-08-04 18:23:59 +0400

    adding navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <GooglePlacesAutocomplete
+          placeholder="Where From"
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+        />
+        <NavOptions />
+      </View>
+    </SafeAreaView>
   );
 };
 
